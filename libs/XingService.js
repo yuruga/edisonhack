1abbb5066ad1fac1bd3cf07b18be30d4b76a14de
maeda <maeda@taki-mex.jp>
2014-10-18 18:28:34 +0900

    no message

---
+++ libs/XingService.js
@@ -0,0 +1,17 @@
+//コンストラクタ
+var XingService = function(){
+	//インスタンスプロパティ
+	this.sample_instance_prop = "value";
+}
+//クラスプロパティ
+XingService.sample_class_prop = "value";
+//クラスメソッド
+XingService.sampleClassFunc = function(){
+	
+};
+//インスタンスメソッド
+XingService.prototype = {
+	sampleFunc: function(){
+	
+	}
+};
