5e8f6a90c5ce06b1a3dea8220aa944bb4a4e3559
xlune <ichikawa@xlune.com>
2014-10-18 18:02:34 +0900

    first commit

---
+++ libs/CarService.js
@@ -0,0 +1,17 @@
+//コンストラクタ
+var CarService = function(){
+	//インスタンスプロパティ
+	this.sample_instance_prop = "value";
+}
+//クラスプロパティ
+CarService.sample_class_prop = "value";
+//クラスメソッド
+CarService.sampleClassFunc = function(){
+	
+};
+//インスタンスメソッド
+CarService.prototype = {
+	sampleFunc: function(){
+	
+	}
+};
